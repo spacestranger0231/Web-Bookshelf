aefbf8b46fb88741cb4cda694d6346c6a25cc2bb
Mukovoz T <91981870+spacestranger0231@users.noreply.github.com>
2022-11-28 23:36:54 +0200

    Add files via upload

---
+++ public/background.js
@@ -0,0 +1,10 @@
+const button=document.getElementById('but1');
+const backgraund=()=>{
+    if (document.getElementById('htm').style.backgroundImage="url('https://foodtank.com/wp-content/uploads/2021/07/alfons-morales-YLSwjSy7stw-unsplash.jpg')"){
+          document.getElementById('htm').style.backgroundImage="url('https://dementiajourney.org/wp-content/uploads/2017/10/desktop-wallpaper-books-wallpapers-bookshelf-old-ladder-book-library-vintage-cartoon.jpg')";
+        }
+        else{
+          document.getElementById('htm').style.backgroundImage="url('https://foodtank.com/wp-content/uploads/2021/07/alfons-morales-YLSwjSy7stw-unsplash.jpg')";
+        }
+}
+
